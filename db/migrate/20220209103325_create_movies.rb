8697e402e826f48845f34ecceb084b07b61a3f07
yongjia7 <yongjia.zhang@kellogg.northwestern.edu>
2022-02-09 10:42:11 +0000

    too hard

---
+++ db/migrate/20220209103325_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :year_released
+      t.string :rated
+      t.integer :person_id
+
+      t.timestamps
+    end
+  end
+end
